af15d5b1946a2b360a94126c81faef5abba0df1d
AndreeaLazea <91785714+AndreeaLazea@users.noreply.github.com>
2021-10-11 17:03:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def printMenu():
     print("1. citire lista")
     print("2. cea mai lunga subsecventa in care suma numerelor este numar prim")
     print("3. cea mai lunga subsecventa in care numarul de cifre este in ordine descrescatoare")
-    print("4. iesire")
-
+    print("4. cea mai lunga subsecventa in care produsul numerelor este impar")
+    print("5. iesire")
 
 def citireLista():
     l = []
@@ -11,7 +11,6 @@ def citireLista():
     for i in range(n):
         l.append(int(input("l[ " + str(i) + " ] = ")))
     return l
-
 
 def sum_is_prime(l):
     '''
@@ -30,7 +29,6 @@ def sum_is_prime(l):
                 return False
     return True
 
-
 def get_longest_sum_is_prime(lst: list[int]) -> list[int]:
     '''
     determina cea mai lunga subsecventa a carei suma este un numar prim
@@ -43,7 +41,6 @@ def get_longest_sum_is_prime(lst: list[int]) -> list[int]:
             if sum_is_prime(lst[i:j + 1]) and len(subsecventa_maxima) < len(lst[i: j + 1]):
                 subsecventa_maxima = lst[i:j + 1]
     return subsecventa_maxima
-
 
 def test_get_longest_sum_is_prime():
     assert get_longest_sum_is_prime([2, 1, 1, 1, 1, 1, 1, 1, 3]) == [1, 1, 1, 1, 1, 1, 1]
@@ -73,7 +70,6 @@ def numar_cifre(l):
             return False
     return True
 
-
 def get_longest_digit_count_desc(lst: list[int]) -> list[int]:
     '''
     determina cea mai lunga subsecventa in care numărul de cifre este în ordine descrescătoare.
@@ -92,6 +88,36 @@ def test_get_longest_digit_count_desc():
     assert get_longest_digit_count_desc([]) == []
     assert get_longest_digit_count_desc([4444, 444, 44, 40, 1, 12, 1234]) == [4444, 444, 44, 40, 1]
 
+def produs_impar(l):
+    '''
+    determina daca produsul listei este sau nu un numar impar
+    :param l: o lista de numere intregi
+    :return: True, daca produsul este impar si False daca este par
+    '''
+    prod = 1
+    for x in l:
+        prod = prod * x
+    if prod%2==0:
+        return False
+    return True
+
+def get_longest_product_is_odd(lst: list[int]) -> list[int]:
+    '''
+    determina cea mai lunga subsecventa a carei produs este numar impar
+    :param lst: o lista de nr. intregi
+    :return: returneaza cea mai lunga subsecventa a carei produs este un numar impar
+    '''
+    subsecventa_maxima = []
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if produs_impar(lst[i:j+1]) is True and len(subsecventa_maxima) < len(lst[i:j+1]):
+                subsecventa_maxima = lst[i:j + 1]
+    return subsecventa_maxima
+
+def test_get_longest_product_is_odd():
+    assert get_longest_product_is_odd([1, 3, 5, 0, 0, 7, 6]) == [1, 3, 5]
+    assert get_longest_product_is_odd([]) == []
+    assert get_longest_product_is_odd([5, 7, 10, 3, 3, 3, 3, 0, 9, 9]) == [3, 3, 3, 3]
 
 def main():
     test_get_longest_digit_count_desc()
@@ -106,10 +132,11 @@ def main():
             print(get_longest_sum_is_prime(l))
         elif optiune == '3':
             print(get_longest_digit_count_desc(l))
-        elif optiune == '4':
+        elif optiune == '4' :
+            print(get_longest_product_is_odd(l))
+        elif optiune == '5':
             break
         else:
             print("optiune gresita, reincercati!")
-
 
 main()
